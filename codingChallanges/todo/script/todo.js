bea6e16e88950fbcf6c82eaf562ab79c22f6a6f8
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-03-29 19:24:52 -0500

    3/29

--- codingChallanges/todo/script/todo.js
+++ codingChallanges/todo/script/todo.js
@@ -14,12 +14,13 @@ console.log("item.text:", item.text)
 const rootElement = document.getElementById("main-todo-list")
 console.log("rootElement:", rootElement)
 
-const todoText = document.getElementsByClassName("todo-text")
-console.log("todoText:", todoText)
+// const todoText = document.getElementsByClassName("todo-text")
+// console.log("todoText:", todoText)
 
+// ------- to do #1
 
-const divElement = document.createElement("divElement")
-divElement.className="toDoDiv"
+const divElement = document.createElement("div")
+divElement.className="todo"
 rootElement.appendChild(divElement)
 
 
@@ -29,9 +30,27 @@ inputElement.type="checkbox"
 rootElement.appendChild(inputElement)
 divElement.appendChild(inputElement)
 
-
 const spanElement = document.createElement("span")
 spanElement.innerHTML = item.text
-inputElement.className="todo-text"
+spanElement.className="todo-text"
 rootElement.appendChild(spanElement)
 divElement.appendChild(spanElement)
+
+// ------- to do #2
+
+const divElement2 = document.createElement("div")
+divElement2.className="todo"
+rootElement.appendChild(divElement2)
+
+
+const inputElement2 = document.createElement("input")
+inputElement2.className="todo-checkbox"
+inputElement2.type="checkbox"
+rootElement.appendChild(inputElement2)
+divElement2.appendChild(inputElement2)
+
+const spanElement2 = document.createElement("span")
+spanElement2.innerHTML = item.text
+spanElement2.className="todo-text"
+rootElement.appendChild(spanElement2)
+divElement2.appendChild(spanElement2)
