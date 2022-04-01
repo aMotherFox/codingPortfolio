b6406dd396ad43e4f14a74322fc310c87b44e551
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-03-31 20:36:28 -0500

    we set women in STEM back at least 10 years

--- codingChallanges/todo/script/todo.js
+++ codingChallanges/todo/script/todo.js
@@ -6,154 +6,25 @@ const todoItems = [
 ];
 console.log("todoItems: ", todoItems)
 
-const item = todoItems.find(item => item.id === 1)
-console.log("item.text:", item.text)
-const item2 = todoItems.find(item => item.id === 2)
-console.log("item2.text:", item2.text)
-const item3 = todoItems.find(item => item.id === 3)
-console.log("item3.text:", item3.text)
-const item4 = todoItems.find(item => item.id === 4)
-console.log("item4.text:", item4.text)
-
 window.onload = onPageLoad();
 
 function onPageLoad () {
     document.querySelector(".todo-checkbox").checked = true;
 }
 
-const rootElement = document.getElementById("main-todo-list")
-console.log("rootElement:", rootElement)
 
-// const todoText = document.getElementsByClassName("todo-text")
-// console.log("todoText:", todoText)
+// --------------- DELETED "delete me"
+const step1 = document.querySelector(".todo-text")
+step1.setAttribute("id", "delete-me")
 
-// ------- to do #1
+let getDeleted = document.getElementById("delete-me")
+console.log("get delete", getDeleted)
+if (getDeleted.parentNode) {
+  getDeleted.parentNode.removeChild(getDeleted)
+}
 
-const divElement = document.createElement("div")
-divElement.className="todo"
-rootElement.appendChild(divElement)
-
-
-const inputElement = document.createElement("input")
-inputElement.className="todo-checkbox"
-inputElement.type="checkbox"
-rootElement.appendChild(inputElement)
-divElement.appendChild(inputElement)
-
-const spanElement = document.createElement("span")
-spanElement.innerHTML = item.text
-spanElement.className="todo-text"
-rootElement.appendChild(spanElement)
-divElement.appendChild(spanElement)
-
-// ------- to do #2
-
-const divElement2 = document.createElement("div")
-divElement2.className="todo"
-rootElement.appendChild(divElement2)
+// ----------------------
 
 
-const inputElement2 = document.createElement("input")
-inputElement2.className="todo-checkbox"
-inputElement2.type="checkbox"
-rootElement.appendChild(inputElement2)
-divElement2.appendChild(inputElement2)
-
-const spanElement2 = document.createElement("span")
-spanElement2.innerHTML = item2.text
-spanElement2.className="todo-text"
-rootElement.appendChild(spanElement2)
-divElement2.appendChild(spanElement2)
-
-// ------- to do #3
-
-const divElement3 = document.createElement("div")
-divElement3.className="todo"
-rootElement.appendChild(divElement3)
-
-
-const inputElement3 = document.createElement("input")
-inputElement3.className="todo-checkbox"
-inputElement3.type="checkbox"
-rootElement.appendChild(inputElement3)
-divElement3.appendChild(inputElement3)
-
-const spanElement3 = document.createElement("span")
-spanElement3.innerHTML = item3.text
-spanElement3.className="todo-text"
-rootElement.appendChild(spanElement3)
-divElement3.appendChild(spanElement3)
-
-// ------- to do #4
-
-const divElement4 = document.createElement("div")
-divElement4.className="todo"
-rootElement.appendChild(divElement4)
-
-
-const inputElement4 = document.createElement("input")
-inputElement4.className="todo-checkbox"
-inputElement4.type="checkbox"
-rootElement.appendChild(inputElement4)
-divElement4.appendChild(inputElement4)
-
-const spanElement4 = document.createElement("span")
-spanElement4.innerHTML = item4.text
-spanElement4.className="todo-text"
-rootElement.appendChild(spanElement4)
-divElement4.appendChild(spanElement4)
-
-
-// ------- mark as complete
-// event.target.value
-
-
-
-
-let allCheckBox = document.querySelectorAll("main-todo-list")
-
-  allCheckBox.forEach((checkbox) => { 
-    checkbox.addEventListener('change', (event) => {
-      if (document.querySelector('.checkbox').checked === true) {
-        
-      }
-      return checkbox(".todo.complete.hidden", ".todo.complete")
-    })
-  })
-
-
-
-// function completingTast(todoVar){
-//     let taskName = todoVar.rootElement
-//     if(todoVar.checked){
-//         taskName.add("todo-complete", "todo-complete-hidden")
-//     } 
-    
-// }
-// completingTast.appendChild(rootElement)
-
-
-
-// document.getElementById("main-todo-list").onclick = function(todoItems) {
-//     const checkboxes = document.querySelectorAll("todo-checkbox");
-//     if true {
-//         todoVar.innerHTML=(todo.complete)
-//         todoVar.className=(todo.complete.hidden)
-//         rootElement.appendChild(todoVar);
-//         todoVar.appendChild(todoVar)
-//     }
-// } event.target.value
-
-
-// todoVar.addEventListener('click', event => {
-//     if (event.target.classList.contains('js-tick')) {
-//       const itemKey = event.target.parentElement.dataset.key;
-//       toggleDone(itemKey);
-//     }
-//   });
-// function markingComplete(todoVar){
-//     if todoVar.onclick = true
-//     })
-//     return ("todo-complete", "todo-complete-hidden")}
 
 
