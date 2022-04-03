64f148e5848dc5741e7c30b043fbdb88fc998cdc
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-04-03 18:56:42 -0500

    pain

--- codingChallanges/todo/script/todo.js
+++ codingChallanges/todo/script/todo.js
@@ -6,25 +6,84 @@ const todoItems = [
 ];
 console.log("todoItems: ", todoItems)
 
-window.onload = onPageLoad();
+//-------------------- checkmark when page loads
+// window.onload = onPageLoad();
 
-function onPageLoad () {
-    document.querySelector(".todo-checkbox").checked = true;
-}
+// function onPageLoad () {
+//     document.querySelector(".todo-checkbox").checked = true;
+// }
 
+//----------------------------------- todo items
+const item = todoItems.find(item => item.id === 1)
+console.log("item 1: ", item.text)
 
-// --------------- DELETED "delete me"
-const step1 = document.querySelector(".todo-text")
+const item2 = todoItems.find(item => item.id === 2)
+console.log("item 2: ", item2.text)
+
+const item3 = todoItems.find(item => item.id === 3)
+console.log("item 3: ", item3.text)
+
+const item4 = todoItems.find(item => item.id === 4)
+console.log("item 4: ", item4.text)
+
+// // ---------------------------------- DELETED "delete me"
+// const deleteMe = document.querySelector(".todo-text")
+// deleteMe.setAttribute("id", "delete-me")
+
+// let getDeleted = document.getElementById("delete-me")
+// console.log("get delete", getDeleted)
+// if (getDeleted.parentNode) {
+//   getDeleted.parentNode.removeChild(getDeleted)
+// }
+
+// -------------------------------------- delete todo list
+// const step1 = document.querySelector("todo")
+// step1.setAttribute("id", "delete-me")
+
+// let deleting = document.getElementById("delete-me")
+// if 
+
+const step1 = document.querySelector(".todo")
 step1.setAttribute("id", "delete-me")
 
 let getDeleted = document.getElementById("delete-me")
-console.log("get delete", getDeleted)
+console.log("get deleted", getDeleted)
 if (getDeleted.parentNode) {
-  getDeleted.parentNode.removeChild(getDeleted)
+    getDeleted.parentNode.removeChild(getDeleted)
 }
 
-// ----------------------
 
+// -------root element to attach divs for main-todo-list------
+
+ const rootElement = document.getElementById("main-todo-list")
+ console.log("rootElement: ", rootElement)
+
+///--------- todo function ---------------
+
+
+const list= document.getElementById("main-todo-list")
+const lineThrough = document.getElementsByClassName("todo complete")
+console.log("linethrough", lineThrough)
+function addToDo(toDo, id, completed){
+
+    //const DONE = completed ? checked : unchecked 
+    const LINE = completed ? lineThrough : "";
+
+    const item = `
+                <div class="todo">
+                <input type="checkbox" class=".todo-checkbox"/>
+                <span class ="todo-text ${LINE}">${toDo}</span>
+                </div>
+            `
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position,item)
+}
+
+addToDo(item.text)
+addToDo(item2.text)
+addToDo(item3.text)
+addToDo(item4.text)
 
 
 
