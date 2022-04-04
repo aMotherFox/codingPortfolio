5144528ff5312555f12fbabaf425821ed0644d1f
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-04-04 17:13:18 -0500

    redo failed

--- codingChallanges/todo/script/todo.js
+++ codingChallanges/todo/script/todo.js
@@ -6,19 +6,19 @@ const todoItems = [
 ];
 console.log("todoItems: ", todoItems)
 
-const rootElement = document.getElementById("main-todo-list")
-console.log("rootElement: ", rootElement)
+// const rootElement = document.getElementById("main-todo-list")
+// console.log("rootElement: ", rootElement)
 
-const insideText = document.createElement("todo-text")
-insideText.innerHTML = ""
-rootElement.appendChild(insideText)
+// const insideText = document.createElement("todo-text")
+// insideText.innerHTML = ""
+// rootElement.appendChild(insideText)
 
-// ---------------- framework
+// // ---------------- framework
 
-const item = document.createElement("div")
-item.className = "main-todo-list"
-item.appendChild(insideText)
-rootElement.appendChild(item)
+// const item = document.createElement("div")
+// item.className = "main-todo-list"
+// item.appendChild(insideText)
+// rootElement.appendChild(item)
 
 // const item2 = todoItems.find(item => item.id === 2)
 // console.log("item 2: ", item2.text)
@@ -71,3 +71,86 @@ rootElement.appendChild(item)
 //     return listItem;
 // }
 // const tasksReturned = creatingNewTaskElement(todoItems)
+
+
+
+//----------------------------------- todo items
+const item = todoItems.find(item => item.id === 1)
+console.log("item 1: ", item.text)
+
+const item2 = todoItems.find(item => item.id === 2)
+console.log("item 2: ", item2.text)
+
+const item3 = todoItems.find(item => item.id === 3)
+console.log("item 3: ", item3.text)
+
+const item4 = todoItems.find(item => item.id === 4)
+console.log("item 4: ", item4.text)
+
+
+
+// -------root element to attach divs for main-todo-list------
+
+const rootElement = document.getElementById("main-todo-list")
+console.log("rootElement: ", rootElement)
+
+
+
+// // ---------------------------------- DELETED "delete me"
+// const deleteMe = document.querySelector(".todo-text")
+// deleteMe.setAttribute("id", "delete-me")
+
+// let getDeleted = document.getElementById("delete-me")
+// console.log("get delete", getDeleted)
+// if (getDeleted.parentNode) {
+//   getDeleted.parentNode.removeChild(getDeleted)
+// }
+
+// -------------------------------------- delete todo list
+
+const step1 = document.querySelector(".todo")
+step1.setAttribute("id","delete-me")
+console.log("step1", step1)
+
+let getDeleted = document.getElementById("delete-me")
+console.log("get deleted", getDeleted)
+if (getDeleted.parentNode) {
+    getDeleted.parentNode.removeChild(getDeleted)
+}
+console.log("getDeleted", getDeleted)
+
+///--------- add back list ---------------
+
+
+const list= document.getElementById("main-todo-list")
+const lineThrough = document.getElementsByClassName(".todo-complete")
+console.log("linethrough", lineThrough)
+function addToDo(toDo, id, completed){
+
+    const LINE = completed ? lineThrough : "";
+
+    const item = `
+                <div class="todo">
+                <input type="checkbox" class=".todo-checkbox"/>
+                <span class ="todo-text ${LINE}">${toDo}</span>
+                </div>
+            `
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position,item)
+}
+
+addToDo(item.text)
+addToDo(item2.text)
+addToDo(item3.text)
+addToDo(item4.text)
+
+
+
+
+//-------------------- checkmark when page loads
+window.onload = onPageLoad();
+
+function onPageLoad () {
+    document.querySelector(".todo-checkbox").checked = true;
+} 
