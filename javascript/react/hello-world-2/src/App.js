7adb0112f486f2b94c1c895c4e714bbe1fba930c
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-04-08 16:14:07 -0500

    components

--- javascript/react/hello-world-2/src/App.js
+++ javascript/react/hello-world-2/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 id="header" className="intro">Hello World</h1>
     </div>
   )
 
--- javascript/react/jsx-components-props/src/index.js
+++ javascript/react/jsx-components-props/src/index.js
@@ -15,3 +15,34 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const superPeople = [
+  {
+    name: 'Thanos',
+    power: 'The Snap!'
+  },
+  {
+    name: 'Iron Man',
+    power: 'Intelligence x 3000'
+  },
+  {
+    name: 'Dr Strange',
+    power: 'the Time Stone'
+  }
+];
+
+const restaurants = ['Cosmic Stones Wings', 'BK', 'Rye of Agamotto']
+
+const MainComponent = (props) => (
+  <div>
+    <SuperPeople
+      superPeople={superPeople}
+    />
+    <Restaurants
+      restaurants={restaurants}
+    />
+  </div>
+)
+
+export default MainComponent;
