48bf91d6d7a50a32d21f3b295f08dcc7d29b8fa5
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-05-28 16:31:42 -0500

    RPG game

--- java/adventure/RolePlayingGame.java
+++ java/adventure/RolePlayingGame.java
@@ -37,17 +37,17 @@ public class RolePlayingGame {
         String userName = scanner.nextLine(); //stage 1
         System.out.println(" 'Hello, " + userName + "' the fairy giggles. ");
         System.out.print(" You ask Victoria if there is anything she needs from you. ");
-        System.out.print(" 'I'm on my way to the midsummer picnic in the Korok Forest! All the bunnies, mice, squirrels, and fawns are waiting for me to arrive and use my magic to summon the fireflies for the band's performance' Victoria said proudly. ");
+        System.out.print(" 'I'm on my way to the midsummer picnic in the Korok Forest! All the bunnies, mice, squirrels, and fawns are waiting for me to arrive and use my magic to summon the fireflies for the band's performance' Victoria said proudly. Even though you just met the little fairy, you already new it made perfect sense that she would be so popular and beloved by the community. She is as sweet as she is beautiful. ");
         System.out.print(" 'I heard you're a kind hearted person who would help a little fairy like me. Is that true?' (true/false)");
 
         Boolean willHelp = scanner.nextBoolean(); //stage 2
         if (willHelp == true){
-            System.out.println(" 'I'm so happy! so it is " + willHelp + " that you're a kind hearted person!' Victoria smiled with happy tears in her eyes. ");
+            System.out.println(" 'I'm so happy! so it is " + willHelp + " that you're a kind hearted person!' Victoria smiled with happy tears in her eyes. 'Thank you" + userName + " ' ");
         } else {
             System.out.println(" Victoria stares at you blankly. 'Then perish' she says ominously as your vision goes dark. ");
             System.exit(1);
         }
-        System.out.print(" You stand, brushing the grass blades off your clothes as Victoria perches on your shoulder and you begin walking in the direction of Korok forest. ");
+        System.out.print(" You stand, brushing the grass blades off your clothes as Victoria perches on your shoulder and you begin walking in the direction of Korok forest, the fairy's fluffy ponytail bouncing with each step. ");
         System.out.print(" Unsure if you've actually made it out of the Great Hyrule Forest, you come to a path split in 3 directions. 'Which way should we go? Path 1, 2, or 3?', Victoria asks. ");
 
         Integer userNumberPick = scanner.nextInt(); //stage 3
@@ -58,15 +58,15 @@ public class RolePlayingGame {
         // } else {
         //     System.out.println(" You wander down the bright scenic path, stopping to admire the shrines you pass. ");
         // }
-        System.out.println(" 'You can tell we're in Korok Forest because of the shrines!' Victoria said cheerfully. ");
+        System.out.println(" 'You can tell we're in Korok Forest because of the shrines!' Victoria said cheerfully. Victoria truly is as wise as she is beautiful. ");
         System.out.println(" She led you to a forest clearing and there sat a pie cart that seemed sized for a child. ");
         System.out.println(" Victoria knocked on the wooden counter and up popped a racoon. 'Victoria!' the racoon said and hugged his fairy friend.  ");
-        System.out.println(" 'Hello Scout!', Victoria said while hugging back. 'This is my friend  " + userName + " , I'd like to get some pies as we're headed to the midsummer picnic!' ");
+        System.out.println(" 'Hello Scout!', Victoria said while hugging back. 'This is my friend  " + userName + " , I'd like to get some pies as we're headed to the midsummer picnic!' Victoria truly is as generous as she is beautiful. ");
 
         //scanner here for pie flavor array, choice called pieFlavor
         String pieFlavor = scanner.nextLine(); //stage 4
         System.out.println(" Victoria praises your choice of pie flavor. She buys one of each except for " + pieFlavor + " which she buys two. You pack the pies away into your satchel and continue your journey as Victoria perches again on your shoulder. ");
-        System.out.println(" Victoria seems perfectly content, calling hellos to the butterflies as you both pass them by on the forest path. But you have noticed the forest noises have slowly gotten quieter and quieter. ");
+        System.out.println(" Victoria seems perfectly content, calling hellos to the butterflies as you both pass them by on the forest path. But you have noticed the forest noises have slowly gotten quieter and quieter. You become on-edge, as it would be horrible if something bad happened to the fairest fairy in all the land. ");
         System.out.println(" Suddenly, it is still and silent. You open your mouth to ask Victoria if she senses anything strange, when from the shadows; A GIANT BROWN WOLF JUMPS OUT AT YOU!!! ");
         System.out.println(" You have a split second to make a decision!! Fight or flight!! Is it true or false that you are a fighter? (true/false)");
 
@@ -91,7 +91,7 @@ public class RolePlayingGame {
         Integer milesWalked = scanner.nextInt(); //stage 6
         System.out.println(" Bucky gasped at your response. 'You mean to say you've walked " + milesWalked + " miles?? You two need some rest! Here, hop on! '. "); 
         System.out.println(" You saddle onto Bucky's back and he bounds off into the forest. Thanks to him, you all swiftly arrive at the picnic! It is in the clearing where the Master Sword once stood! ");
-        System.out.println(" Victoria flutters off your shoulder and onto the ground, frolicking forwards to her friends. All the small critters ran to her as they exchanged hugs. "); 
+        System.out.println(" Victoria flutters off your shoulder and onto the ground, frolicking forwards to her friends. All the small critters ran to her as they exchanged hugs. She is as popular as she is beautiful. Glitter sparkles with her every movement."); 
         System.out.println(" The festivies go on for a few hours until the sky turns to twilight. Victoria soars into the air and calls all the fireflies! ");
         System.out.println(" Bucky takes the stage and the party goes silent other than some giggles and murmurs. 'Welcome to the annual midsummer picnic! I'm so glad we could be joined by everyone, and-' ");
         System.out.println(" 'NOT EVERYONE!!' a voice roared out. You turned to look as the party filled with screams of terror, and in the tree branches you saw him; King of the Tigers, Rocky!! ");
@@ -111,17 +111,17 @@ public class RolePlayingGame {
             System.out.println(" You snatch Rocky out of the air and hold him by the scruff so he goes limp. ");
         }
         System.out.println(" 'How?? How have you disarmed me??' Rocky hissed. ");
-        System.out.println(" Victoria flutters forward. 'I believe my magic can cure him! Let me try!' She implored ");
-        System.out.println(" Do you think it's true Rocky can be reformed? (true/false)");
+        System.out.println(" Victoria flutters forward. 'I believe my magic can cure him! Let me try!' She implored. She is as merciful as she is beautiful! ");
+        System.out.println(" Do you think it's true Rocky can be reformed? (Yes/No)");
 
         //scanner here for Rocky boolean, choice called saveRocky
-        Boolean saveRocky = scanner.nextBoolean(); //stage 8
-        if (saveRocky == false){
+        String saveRocky = scanner.nextLine(); //stage 8
+        if (saveRocky == "No"){
             System.out.println(" You decide it is too dangerous. Instead, you banish Rocky to the Lost Woods. Unfortunately you don't have the authority for that, so everyone just ignores Rocky until he feels really awkward and leaves.  ");
             System.out.println(" The party continues on! Go have fun! ");
             System.exit(1);
         } else {
-            System.out.println(" Victoria sprinkles her fairy glitter onto Rocky. You cannot help but notice how stylish and talented she looks while doing it. ");
+            System.out.println(" Victoria sprinkles her fairy glitter onto Rocky. You cannot help but notice how stylish and talented she looks while doing it. If only every citizen of Hyrule could be in her prescence. ");
         }
         System.out.println(" Rocky begins glowing before his fur returns to it's normal grey color. Victoria had done it! She cured Rocky of his misogyny! ");
         System.out.println(" Will you accept Rocky's apology? (Yes/No)");
@@ -135,14 +135,30 @@ public class RolePlayingGame {
 
         String rockyApology = scanner.nextLine(); //stage 9
         RolePlayClass.sorryRocky(rockyApology);
-        // if (rockyApology == "No"){
-        //     System.out.println(" You suggest to Rocky that before he re-joins society as a productive member, first he should get some therapy. Maybe he can come to the next annual midsummer picnic.  ");
-        //     System.exit(1);
-        // } else {
-        //     System.out.println(" The more the merrier! Now that Rocky is free from the shackles of the patriarchy, he's ready to be a productive member of society! ");
-        // }
+        if (rockyApology == "No"){
+            System.out.println(" You suggest to Rocky that before he re-joins society as a productive member, first he should get some therapy. Maybe he can come to the next annual midsummer picnic.  ");
+            //System.exit(1);
+        } else {
+            System.out.println(" The more the merrier! Now that Rocky is free from the shackles of the patriarchy, he's ready to be a productive member of society! ");
+        }
+        System.out.println(" The party went on well into the night, until the moon was peaked directly above them in the sky. You looked around in confusion as Victoria floated above everyone's head. ");
+        System.out.println(" 'It's time!' Victoria called out. Your bewildement only grew as everyone took a knee, facing the stage. Suddenly, you heard it. Loud, heavy steps, sending shockwaves through the ground. Something was coming. ");
+        System.out.println(" The largest of the fawn pulled out a throne from backstage, quickly scampering off to also kneel with the crowd. From the parted trees, you saw it; a silent creature taking slow step after step, sending vibrations with the force of its weight. ");
+        System.out.println(" It's white fur stood in sharp contrast to the red royal cloak and golden crown. It climbed the stairs to the stage at a snail's pace, and trudged to the throne, sending a minor earthquake through the half mile radius as it plopped down. ");
+        System.out.println("The creature stared into the crowd with a blank face, unseeing to it's subjects. A minute of tense, defeaning silence shrowded the gathering, until finally, the creature on the throne opened it's mouth wide. ");
+        System.out.println(" 'NIEVE HAS SPOKEN' Victoria cried and all the party guests grabbed armfuls of food, pouring it directly into the creature's mouth, unneeding to chew and swallowing whole. You stared frozen in shock and terror as this 'Nieve' gulped down hundreds of pounds of food, white fur becoming slathered in sauces, juices, and grease. ");
+        System.out.println(" After a long ritual, the beast is finally satiated. Victoria delicately collapses to the ground in exhaustion, her face flushed innocently with exertion. You scoop her up in your hands and bring her to eye level. ");
+        System.out.println("Do you ask Victoria what this ritual was, and who this Lord was? (Yes/No)");
 
-
+        String nieve = scanner.nextLine(); //stage 10
+        if (nieve == "No"){
+            System.out.println(" Eventually the creature burps loudly, almost bursting everyone's ear drums, and waddles slowly back into the forest. ");
+            //System.exit(1);
+        } else {
+            System.out.println(" 'That's Nieve, my cat. It is her birthday tomorrow so we promised to do this for her. She's not actually royalty, just extremely spoiled.' VICTORIA TRULY IS AS COMPASSIONATE AS SHE IS BEAUTIFUL. ");
+        }
+        System.out.println("As all good things must, the party comes to an end. Victoria's large, sparkly eyes peer up at you demurely as you try to say goodbye. It is increadibly hard as you know you may never meet someone as pure and beautiful as her again. She senses your hesitation and gives your thumb a big hug. She then looks at you, and gives you a wink and finger heart. Your vision begins to darken as her magic transports you home. ");
+        
     }
 
     // private static void something() {
