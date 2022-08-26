90613a1d48dc1efbf044e24a71fa15d320b096a3
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-08-25 19:37:49 -0500

    customer APIs

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/customer/CustomerService.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/customer/CustomerService.java
@@ -1,51 +1,22 @@
 package com.my.FoodTruckApp.customer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service //annotation that tells spring THIS IS OUR SERVICE
 @RequiredArgsConstructor //necessary for the dependency injection to the repository
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
     private final JdbcTemplate jdbcTemplate;
-
-
-    //----------------- get list of customers -------------------------
-    public List<Customer> listOfCustomers(){
-        List<Customer> customers = customerRepository.getAllCustomers();
-        System.out.println("this is ALL!! the Customers" +  customers);
-        return customers;
-    }
-
-    //----------------- get customer by ID -------------------------
-    public Optional<Customer> getCustomerById(@PathVariable Integer id) {
-        ArrayList<Customer> customers = customerRepository.getAllCustomers();
-        System.out.println("getting customers by their id: " + id);
-        Optional<Customer> customerById = customers.stream().filter(customer -> customer.getId() == id).findFirst();
-        return customerById;
-    }
-    //-------------- create new customer -----------------------
-
-    // HARDCODED VERSION!!! v
-//    public String createNewCustomer(@RequestBody CustomerRequestBody customerRequestBody){
-//        String sql = "INSERT INTO customer(id, first_name,last_name) VALUES(DEFAULT, 'Bently','Pruitt')";
-//        System.out.println("--------------------INSIDE CREATENEWCUSTOMER" + sql);
-//        Integer rows = jdbcTemplate.update(sql);
-//        if(rows > 0){
-//            System.out.println("A new row has been inserted!!! (HARDCODED)");
-//        }
-//        return sql;
-//    }
-    //
 
     //-------------- create new customer -----------------------
 
@@ -59,4 +30,30 @@ public class CustomerService {
         }
         return "CREATING A CUSTOMER WORKED";
     }
+
+    //-------------- get customers by id -----------------------
+    public Customer gettingCustomersById(@PathVariable Integer id) {
+        String sql = "SELECT * FROM customer WHERE id = ?";
+        try {
+            Customer customerById = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Customer.class), id);
+            //queryForObject -> TRY
+            System.out.println("customer by ID" + customerById);
+            return customerById;
+        } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
+            System.out.println("incorrect id");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //-------------- get ALL customers -----------------------
+    public List<Customer> gettingListOfCustomers() {
+        String sql = "SELECT * FROM CUSTOMER";
+        List<Customer> customers = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Customer.class));
+        return customers;
+    }
+    //-------------- delete customer -----------------------
+//    public String deleteCustomerById(Integer id) {
+//        String sql = "SELECT * FROM customer WHERE id = ?";
+//    }
+
 }
