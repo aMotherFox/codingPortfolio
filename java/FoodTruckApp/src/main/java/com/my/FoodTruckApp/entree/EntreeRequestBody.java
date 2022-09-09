ff427b3953b707bc6ba337982890c8657cc1daaf
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-09-09 17:53:37 -0500

    finished making CRD APIs for entree (#11)
    * finished making CRD APIs for entree
    
    * updated requestBody per PR feedback

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/entree/EntreeRequestBody.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/entree/EntreeRequestBody.java
@@ -1,15 +1,12 @@
 package com.my.FoodTruckApp.entree;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor//annotation does the all args constructor
-@NoArgsConstructor//annotation does the no args constructor
-@Data //has all the getters, setters, and toString built in
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EntreeRequestBody {
-    private String entreeSize;
-    private String entreeFlavor;
-    private String entreePairedMeal;
-    private Integer entreePrice;
+    private String name;
+    private Integer price;
 }
