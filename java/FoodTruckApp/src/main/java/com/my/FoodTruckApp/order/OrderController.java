b6d93be493aa8ea4e803708c2984e946e0ee3d71
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-09-21 18:15:07 -0500

    separated logic for order

--- java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/order/OrderController.java
+++ java/FoodTruckApp/src/main/java/com/my/FoodTruckApp/order/OrderController.java
@@ -16,7 +16,6 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    //------------------------get list of orders-----------------------------------------------------------------------------------------------------------
     @GetMapping("/orders")
     public ArrayList<Order> getListOfOrders() {
         return orderService.getListOfOrders();
@@ -27,32 +26,11 @@ public class OrderController {
 //    public Order getOrderById(@PathVariable Integer id) {
 //        return orderService.getOrderById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 //    }
-//    //------------------------create order-----------------------------------------------------------------------------------------------------------
     @PostMapping("/orders")
     public OrderDTO createOrder(@RequestBody NewOrderRequestBody newOrderRequestBody) {
         log.info("Creating an order with newOrderRequestBody: " + newOrderRequestBody);
         return orderService.createOrder(newOrderRequestBody);
     }
 
-    //DTO makes it so only certain columns from database are returned
-    //i.e. database as name, username, email, phone number columns, you want to only return name & username for security reasons
-    //DTO connects with the network layer, not the database layer
-    //DTO to entity converter is in CONTROLLER level
-
-    //DTO = data transfer object
-    //order is a class (PROPER DEFINITION)
-    //class is the empty template for an object
-    //object is an instance of a class
-    //model another word for class, models have FIELDS
-    //every class that you want to instanciate should be a model
-    //entity is a model, specifically usually a model that is representation of a table
-    //will not always be completely identical to the table (i.e firstName in java vs first_name in database)
-    //pojo = Plain Old Java Object
-    //pojo is a model
-    //used to have to do mapping yourself
-    //pojo was a step to instanciate objects you had to map yourself
-    //pojo isn't used very much, now we use DTO
-    //DTOs used mostly for creating a model that is not an entity;
-    //when you have an entity tht doesn't have all the fields you want,
 
 }
